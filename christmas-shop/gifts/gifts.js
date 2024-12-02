edc5c3f11de89b19f22efef168934ea5f2a12002
Tatiana B <bodtata@yandex.ru>
2024-12-02 17:39:16 +0100

    feat: add burger menu on gifts page

--- christmas-shop/gifts/gifts.js
+++ christmas-shop/gifts/gifts.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const mediaQueryList = window.matchMedia("(max-width: 768px)");
+
+function showAlert() {
+
+}
+
+mediaQueryList.addEventListener("change", showAlert);
+
+// add burger-menu animation
+const body = document.body;
+const burgerBtn = document.querySelector('.menu_burger');
+const burgerMenu = document.querySelector('nav');
+
+function showMenu() {
+    burgerBtn.classList.toggle('active');
+    burgerMenu.classList.toggle('hidden');
+    body.classList.toggle('overflow')
+}
+
+burgerBtn.addEventListener('click', showMenu);
+
+
--- christmas-shop/script.js
+++ christmas-shop/script.js
@@ -11,13 +11,14 @@ function showAlert() {
 mediaQueryList.addEventListener("change", showAlert);
 
 // add burger-menu animation
-
+const body = document.body;
 const burgerBtn = document.querySelector('.menu_burger');
 const burgerMenu = document.querySelector('nav');
 
 function showMenu() {
     burgerBtn.classList.toggle('active');
-    burgerMenu.classList.toggle('hidden')
+    burgerMenu.classList.toggle('hidden');
+    body.classList.toggle('overflow')
 }
 
 burgerBtn.addEventListener('click', showMenu);
