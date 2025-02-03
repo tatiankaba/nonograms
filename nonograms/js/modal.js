e1bb9b729ffe8d3d2be4388e2364f1f390c01350
Tatiana B <bodtata@yandex.ru>
2025-02-03 20:15:02 +0100

    feat: add modal window

---
+++ nonograms/js/modal.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const body = document.body;
+const overlay = document.createElement('div');
+const modal = document. createElement('div');
+const closeBtn = document.createElement('div');
+
