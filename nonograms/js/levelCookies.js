0e75e5c0402888d84a8c7ad1f6c9eb4b8b0a7298
Tatiana B <bodtata@yandex.ru>
2025-02-11 14:31:27 +0100

    feat: add level block

---
+++ nonograms/js/levelCookies.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+export const levelWrapperBlock = document.createElement('div');
+levelWrapperBlock.classList.add('level-wrapper-block')
+const labelsBlock = document.createElement('div');
+labelsBlock.classList.add('labels-block')
+
+
+const levelInscription = document.createElement('div');
+levelInscription.textContent = 'Choose your level cookie:'
+
+const easyLevelInput = document.createElement('input');
+easyLevelInput.name = 'levelCookies';
+easyLevelInput.id = 'easy';
+easyLevelInput.value = 'simple';
+easyLevelInput.type = 'radio'
+easyLevelInput.classList.add('level-input');
+
+const easyLevelLabel = document.createElement('label');
+easyLevelLabel.for = 'easy';
+easyLevelLabel.classList.add('level-label', 'easy');
+easyLevelLabel.append(easyLevelInput);
+easyLevelLabel.setAttribute('title', '5*5')
+
+
+const mediumLevelInput = document.createElement('input');
+mediumLevelInput.name = 'levelCookies';
+mediumLevelInput.id = 'medium';
+mediumLevelInput.value = 'medium';
+mediumLevelInput.type = 'radio'
+mediumLevelInput.classList.add('level-input');
+
+const mediumLevelLabel = document.createElement('label');
+mediumLevelLabel.for = 'medium';
+mediumLevelLabel.classList.add('level-label', 'medium');
+mediumLevelLabel.append(mediumLevelInput);
+mediumLevelLabel.setAttribute('title', '10*10');
+
+
+const HardLevelInput = document.createElement('input');
+HardLevelInput.name = 'levelCookies';
+HardLevelInput.id = 'hard';
+HardLevelInput.value = 'hard';
+HardLevelInput.type = 'radio'
+HardLevelInput.classList.add('level-input');
+
+const hardLevelLabel = document.createElement('label');
+hardLevelLabel.for = 'hard';
+hardLevelLabel.classList.add('level-label', 'hard');
+hardLevelLabel.append(HardLevelInput);
+hardLevelLabel.setAttribute('title', '15*15');
+
+levelWrapperBlock.append(levelInscription, labelsBlock);
+labelsBlock.append(easyLevelLabel, mediumLevelLabel, hardLevelLabel);
+
+
+
+
--- nonograms/js/script.js
+++ nonograms/js/script.js
@@ -8,7 +8,8 @@ import {startGame, gameContainer} from './gameField.js';
 import { templatesWrapper, gameNameTitleBlock} from './templates.js';
 import { logo } from './logo.js';
 import { timeBlock } from './timeCount.js';
-import { templateObj } from './templateObj.js'
+import { templateObj } from './templateObj.js';
+import { levelWrapperBlock} from './levelCookies.js'
 
 const body = document.body;
 
@@ -16,10 +17,11 @@ const body = document.body;
 body.append(logo);
 body.append(timeBlock);
 body.append(header);
+body.append(levelWrapperBlock)
 body.append(templatesWrapper);
 body.append(gameNameTitleBlock)
 body.append(gameContainer);
-startGame(templateObj.medium.postcard)
+startGame(templateObj.simple.figure)
 
 
 
